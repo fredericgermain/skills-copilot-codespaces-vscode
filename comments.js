7b5cd74f03ff36d6584e50e1408cf9020e3c1b0c
fredericgermain <frederic.germain@gmail.com>
2024-06-21 15:47:36 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,2 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file that you created in the previous task. The comments.html file should load the comments.js file that you created in the previous task.git add comments.js
